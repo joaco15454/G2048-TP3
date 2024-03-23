893b00e43e8e269e6ebb8f89573cbd2655fbdd5e
Joaco Hernandez <joacohernandez157@gmail.com>
2024-03-23 13:05:47 -0300

    Creacion de tablero e implementacion de algunas funciones

--- Negocio/src/negocio.java
+++ Negocio/src/negocio.java
@@ -1,9 +1,26 @@
 
 public class negocio {
+	private static boolean[][] tablero;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola");
 	}
 
+	public static void inciarTablero() {
+		tablero = new boolean[4][4]; 
+		agregarRandom();
+	}
+	public static void agregarRandom() {
+		for (int i = 0; i < tablero.length; i++) {
+	        for (int j = 0; j < tablero.length; j++) {
+	            if (((int)(Math.random() * 100) + 1) % 2 == 0) {
+	            	tablero[i][j] = true;
+	            } else {
+	            	tablero[i][j] = false;
+	            }
+	        }
+	    }
+
+	}
 }
--- Visual/src/visual.java
+++ Visual/src/visual.java
@@ -1,6 +1,12 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import java.awt.Color;
+import javax.swing.border.BevelBorder;
 
 public class visual {
 
@@ -34,8 +40,90 @@ public class visual {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setTitle("2048");
+		frame.setBounds(100, 100, 499, 356);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new GridLayout(4, 4, 0, 0));
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new BevelBorder(BevelBorder.LOWERED, new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255)));
+		panel_1.setBackground(new Color(255, 128, 192));
+		frame.getContentPane().add(panel_1);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBorder(new BevelBorder(BevelBorder.LOWERED, new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255)));
+		panel_3.setBackground(new Color(255, 128, 192));
+		frame.getContentPane().add(panel_3);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new BevelBorder(BevelBorder.LOWERED, new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255)));
+		panel.setBackground(new Color(255, 128, 192));
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JPanel panel_11 = new JPanel();
+		panel_11.setBorder(new BevelBorder(BevelBorder.LOWERED, new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255)));
+		panel_11.setBackground(new Color(255, 128, 192));
+		frame.getContentPane().add(panel_11);
+		
+		JPanel panel_4 = new JPanel();
+		panel_4.setBorder(new BevelBorder(BevelBorder.LOWERED, new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255)));
+		panel_4.setBackground(new Color(255, 128, 192));
+		frame.getContentPane().add(panel_4);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(new Color(255, 128, 192));
+		panel_2.setBorder(new BevelBorder(BevelBorder.LOWERED, new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255)));
+		frame.getContentPane().add(panel_2);
+		
+		JPanel panel_5 = new JPanel();
+		panel_5.setBorder(new BevelBorder(BevelBorder.LOWERED, new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255)));
+		panel_5.setBackground(new Color(255, 128, 192));
+		frame.getContentPane().add(panel_5);
+		
+		JPanel panel_6 = new JPanel();
+		panel_6.setBorder(new BevelBorder(BevelBorder.LOWERED, new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255)));
+		panel_6.setBackground(new Color(255, 128, 192));
+		frame.getContentPane().add(panel_6);
+		
+		JPanel panel_7 = new JPanel();
+		panel_7.setBorder(new BevelBorder(BevelBorder.LOWERED, new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255)));
+		panel_7.setBackground(new Color(255, 128, 192));
+		frame.getContentPane().add(panel_7);
+		
+		JPanel panel_8 = new JPanel();
+		panel_8.setBorder(new BevelBorder(BevelBorder.LOWERED, new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255)));
+		panel_8.setBackground(new Color(255, 128, 192));
+		frame.getContentPane().add(panel_8);
+		
+		JPanel panel_9 = new JPanel();
+		panel_9.setBorder(new BevelBorder(BevelBorder.LOWERED, new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255)));
+		panel_9.setBackground(new Color(255, 128, 192));
+		frame.getContentPane().add(panel_9);
+		
+		JPanel panel_10 = new JPanel();
+		panel_10.setBorder(new BevelBorder(BevelBorder.LOWERED, new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255)));
+		panel_10.setBackground(new Color(255, 128, 192));
+		frame.getContentPane().add(panel_10);
+		
+		JPanel panel_12 = new JPanel();
+		panel_12.setBorder(new BevelBorder(BevelBorder.LOWERED, new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255)));
+		panel_12.setBackground(new Color(255, 128, 192));
+		frame.getContentPane().add(panel_12);
+		
+		JPanel panel_13 = new JPanel();
+		panel_13.setBorder(new BevelBorder(BevelBorder.LOWERED, new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255)));
+		panel_13.setBackground(new Color(255, 128, 192));
+		frame.getContentPane().add(panel_13);
+		
+		JPanel panel_14 = new JPanel();
+		panel_14.setBorder(new BevelBorder(BevelBorder.LOWERED, new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255)));
+		panel_14.setBackground(new Color(255, 128, 192));
+		frame.getContentPane().add(panel_14);
+		
+		JPanel panel_15 = new JPanel();
+		panel_15.setBorder(new BevelBorder(BevelBorder.LOWERED, new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255)));
+		panel_15.setBackground(new Color(255, 128, 192));
+		frame.getContentPane().add(panel_15);
 	}
-
 }
