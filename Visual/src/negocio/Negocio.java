4f36ac378bb3a8df0ee87677a64c00766a1d6e9e
Joaco Hernandez <joacohernandez157@gmail.com>
2024-03-23 16:50:48 -0300

    Maquetacion

---
+++ Visual/src/negocio/Negocio.java
@@ -0,0 +1,26 @@
+package negocio;
+public class Negocio {
+	private static boolean[][] tablero;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola");
+	}
+
+	public static void inciarTablero() {
+		tablero = new boolean[4][4]; 
+		agregarRandom();
+	}
+	public static void agregarRandom() {
+		for (int i = 0; i < tablero.length; i++) {
+	        for (int j = 0; j < tablero.length; j++) {
+	            if (((int)(Math.random() * 100) + 1) % 2 == 0) {
+	            	tablero[i][j] = true;
+	            } else {
+	            	tablero[i][j] = false;
+	            }
+	        }
+	    }
+
+	}
+}
